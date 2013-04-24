29a0ae9013c10b9c79147107c1365f7babaee99d
Constantin Hetagurov <koshtt@gmail.com>
2013-04-24 15:58:58 +0400

    sddfsdfsdfs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testauthorize::Application.config.secret_token = '2c33e99ab3a55d62e440d6fd8350149ef268b900abe3bbb8cb39dd755b1a48f4236f7636f7cd2dbcabbd8bec4a0663ebec9a62297a3b3a023c29a2ee691bce9a'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Testauthorize::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users, :only => [:index, :destroy]
+  root :to => 'users#index'
+end
