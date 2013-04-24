10133df75d52f8a2eb07a7c653bf5521cb3b83f1
Constantin Hetagurov <koshtt@gmail.com>
2013-04-24 17:29:22 +0400

    fwwwefdewd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Testauthorize::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, :only
+  resources :users
   root :to => 'users#index'
 end
