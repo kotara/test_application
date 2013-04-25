48dafceaf5537130d4f8847b1d7a5d2ee505beca
Constantin Hetagurov <koshtt@gmail.com>
2013-04-25 21:38:34 +0400

    blswsss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :authenticate_user!, :set_vk_client
+
   # GET /users
   # GET /users.json
   def index
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @friends = @vk.friends.get(fields: [:first_name, :last_name, :photo_medium])
+    @vk = VkontakteApi::Client.new(access_token.credentials.token)
+  	@friends = @vk.friends.get(fields: [:first_name, :last_name, :photo_medium])
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
@@ -80,9 +81,6 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
-  def set_vk_client
-    @vk = VkontakteApi::Client.new(current_user.authentication_token)
-  end
+
   
 end
