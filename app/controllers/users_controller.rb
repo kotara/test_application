c4740d8457842e3c3cb59d1c97cda9c5e4b52abb
Constantin Hetagurov <koshtt@gmail.com>
2013-04-25 22:06:57 +0400

    fsdsaq

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @vk = VkontakteApi::Client.new(access_token.credentials.token)
+    @vk = VkontakteApi::Client.new(current_user.authentication_token)
   	@friends = @vk.friends.get(fields: [:first_name, :last_name, :photo_medium])
     respond_to do |format|
       format.html # show.html.erb
