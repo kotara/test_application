3788fac848236345a44bdc197a21b49f0a6a9880
Constantin Hetagurov <koshtt@gmail.com>
2013-04-25 23:36:47 +0400

    asa

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
+   authentication_token = request.env['omniauth.auth'].credentials.token 
     @user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+    @user.update_attribute(:authentication_token, authentication_token)
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
